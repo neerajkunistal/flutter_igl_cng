14028f40923d4e4e4b9bccb27d6f0c5ac915da1d
neerajkunistal <neerajk@unistal.com>
2024-07-22 16:35:43 +0530

    add CV and Ci And Amo complaint status update condition

--- lib/feature/amo/viewAmoCimplaint/presentation/widget/view_amo_complaint_item_box_widget.dart
+++ lib/feature/amo/viewAmoCimplaint/presentation/widget/view_amo_complaint_item_box_widget.dart
@@ -175,10 +175,10 @@ class ViewAmoComplaintItemBoxWidget extends StatelessWidget {
     return Align(
       alignment: Alignment.centerRight,
       child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.40,
+        width: MediaQuery.of(context).size.width * 0.43,
         height: MediaQuery.of(context).size.height * 0.07,
         child: ButtonWidget(
-            fontSize: AppFont.font_12,
+            fontSize: AppFont.font_11,
             text: AppString.updateStatus,
             onPressed: () async {
               var res = await showDialog(
