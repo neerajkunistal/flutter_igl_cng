312f4ad5f85179cef57f73d30048375f59e00d29
neerajkunistal <neerajk@unistal.com>
2024-09-18 16:30:24 +0530

    1.	Integrate SU and LCV manager 2.	Filter date wise upto last 3 days 3.	DB station filter. 4.	LCV number wise filter. 5.	Scheduled maintenance penalty throwns (need discussion) 6.	CheckList -> Ok , Not Ok (rename ) 7.	DB station Date a.	Arrived at b.	LCV at MS c.	In-transit MS to DS

--- lib/testing_page.dart
+++ lib/testing_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_igl_cng/ExportFile/app_export_file.dart';
 import 'package:flutter_igl_cng/utils/commonWidgets/data_table_widget.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class TestPage extends StatefulWidget {
@@ -11,48 +14,137 @@ class TestPage extends StatefulWidget {
 
 class _TestPageState extends State<TestPage> {
 
-  final _fixedRowCells = [
-    "Plant",
-    "Plant Name",
-    "Storage Location",
-    "Storage Location Name",
-    "Unrestricted Stock"
-    "Base Unit of Measure"
-  ];
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  String long = "", lat = "";
+  late StreamSubscription<Position> positionStream;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
+  @override
+  void initState() {
+    checkGps();
+    super.initState();
+  }
 
-  final _rowsCells = [
-    ["3010", "IGL  Greater Noida", "0045", "Avinash Em Proje", "5.000", "NO"],
-  ];
+  checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if(servicestatus){
+      permission = await Geolocator.checkPermission();
 
-  final _fixedColCells = [];
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        }else if(permission == LocationPermission.deniedForever){
+          print("'Location permissions are permanently denied");
+        }else{
+          haspermission = true;
+        }
+      }else{
+        haspermission = true;
+      }
+
+      if(haspermission){
+        setState(() {});
+        getLocation();
+      }
+    }else{
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() { });
+  }
+
+  getLocation() async {
+    position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position.longitude); //Output: 80.24599079
+    print(position.latitude); //Output: 29.6593457
+
+    long = position.longitude.toString();
+    lat = position.latitude.toString();
+
+    setState(() {
+      //refresh UI
+    });
+
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 0, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    positionStream = Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen((Position position) {
+      print(position.longitude); //Output: 80.24599079
+      print(position.latitude); //Output: 29.6593457
+      print("Speed Accuracy ${position.speedAccuracy}");
+      long = position.longitude.toString();
+      lat = position.latitude.toString();
+      _add();
+
+      setState(() {
+        //refresh UI on update
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Table(border: TableBorder.all(), children: [
-            const TableRow(children: [
-              Text('Column 1'),
-              Text('Column 2'),
-            ]),
-            TableRow(children: [
-              const Text('Entry 1'),
-              Table(border: TableBorder.all(), children: const [
-                TableRow(children: [
-                  Text('Nested Entry 1'),
-                  Text('Nested Entry 2'),
-                ]),
-                TableRow(children: [
-                  Text('Nested Entry 3'),
-                  Text('Nested Entry 4'),
-                ]),
-              ]),
-            ]),
-          ]),
+        appBar: AppBar(
+            title: const Text("Get GPS Location"),
+            backgroundColor: Colors.redAccent
         ),
+        body: _marker(),
+    );
+  }
+
+  void _add() {
+    var markerIdVal = DateTime.now().timeZoneOffset.toString();
+    final MarkerId markerId = MarkerId(markerIdVal);
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(
+        double.parse(lat),
+        double.parse(long),
+      ),
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+      onTap: () {
+        // _onMarkerTapped(markerId);
+      },
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  Widget _marker() {
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(-33.852, 151.211),
+        zoom: 11.0,
+      ),
+      markers: Set<Marker>.of(markers.values), // YOUR MARKS IN MAP
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    final marker = Marker(
+      markerId: MarkerId('place_name'),
+      position: LatLng(9.669111, 80.014007),
+      // icon: BitmapDescriptor.,
+      infoWindow: InfoWindow(
+        title: 'title',
+        snippet: 'address',
       ),
     );
+
+    setState(() {
+      markers[MarkerId('place_name')] = marker;
+    });
   }
 }
